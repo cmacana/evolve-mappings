b7d6d235b5721cd7c32fda4c6de2ba3896cede0e
cmacana <carlos.macana@zepben.com>
2021-03-16 01:06:30 +1100

    Add StdLineReader.java
    Signed-off-by: cmacana <carlos.macana@zepben.com>

--- src/main/kotlin/com/zepben/evolvemapping/Main.kt
+++ src/main/kotlin/com/zepben/evolvemapping/Main.kt
@@ -1,4 +1,25 @@
 package com.zepben.evolvemapping
 
+import com.zepben.evolvemapping.config.StdLineReader
+import org.slf4j.LoggerFactory
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.text.ParseException
+import kotlin.system.exitProcess
+
+
+private val logger = LoggerFactory.getLogger("xml-rdf-network-importer")
+
+fun main() {
+    val dbPath = "F:\\Data\\EssentialEnergy\\SINCAL\\LineTypesV16.mdb"
+
+    val p = Paths.get(dbPath)
+    print(p.fileName)
+    print(p.toString())
+
+    val stdLineReader = StdLineReader()
+    val records = stdLineReader.readMsAccessStdLineRecords(p)
+
+}
 
 
---
+++ src/main/kotlin/com/zepben/evolvemapping/config/StdLineReader.java
@@ -0,0 +1,68 @@
+package com.zepben.evolvemapping.config;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.Math.round;
+
+public class StdLineReader {
+    public Set<StdLineRecord> readMsAccessStdLineRecords(Path path) throws SQLException, ClassNotFoundException {
+        final Set<StdLineRecord> records = new HashSet<>();
+        // Load driver
+        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        String url = "jdbc:ucanaccess://" + path.toString();
+        try (
+                final Connection connection = DriverManager.getConnection(url);
+                final Statement statement = connection.createStatement();
+                final ResultSet resultSet = statement.executeQuery("SELECT * FROM Stdline")
+        ) {
+            while (resultSet.next()) {
+                records.add(
+                        new StdLineRecord(
+                                resultSet.getInt("Element_ID"),
+                                resultSet.getDouble("r"),
+                                resultSet.getDouble("x"),
+                                resultSet.getDouble("r0"),
+                                resultSet.getDouble("x0"),
+                                resultSet.getDouble("Ith")
+                        ));
+            }
+            return records;
+        }
+    }
+
+    interface Dependencies {
+
+        byte[] readBytes(Path path) throws IOException;
+
+        Set<StdLineRecord> readMsAccesssStdLineRecords(Path path) throws Exception;
+
+        String getMimeType(Path path) throws IOException;
+
+    }
+
+    final static class StdLineRecord {
+
+        final int elementId;
+        final double r;
+        final double x;
+        final double r0;
+        final double x0;
+        final double ith;
+
+        public StdLineRecord(int elementId, double r, double x, double r0, double x0, double ith) {
+            this.elementId = elementId;
+            this.r = r;
+            this.x = x;
+            this.r0 = r0;
+            this.x0 = x0;
+            this.ith = ith;
+        }
+    }
+
+}
